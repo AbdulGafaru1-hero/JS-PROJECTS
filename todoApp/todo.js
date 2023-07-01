7f5aea9b9f43af4721fa1f1759f5edba49b8cff6
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-07-01 10:52:31 +0000

    Ranndom Phone number project started

--- todoApp/todo.js
+++ todoApp/todo.js
@@ -1,20 +1,33 @@
 
 // const addBtn = document.querySelector(".btn")
 
+// const todoList = document.querySelector(".todo-list")
+
+//  function addBtn(){
+//  let todoList = document.querySelector(".todo-list")
+//   let input = document.querySelector(".input-field").value
+//  let li = document.createElement("li");
+//  let t = document.createTextNode(input);
+//  li.appendChild(t);
+//  if (input === ''){ 
+//   alert("You must write something!");
+// } else{
+// todoList.appendChild(li)
+
+// }
+// document.querySelector(".input-field").value = ""
+//  }
+
+
 const todoList = document.querySelector(".todo-list")
 
- function addBtn(){
- let todoList = document.querySelector(".todo-list")
+ function addBtn(){  
   let input = document.querySelector(".input-field").value
- let li = document.createElement("li");
- let t = document.createTextNode(input);
- li.appendChild(t);
- if (input === ''){ 
-  alert("You must write something!");
-} else{
-todoList.appendChild(li)
-
-}
+  if (input === '') {
+    alert("You must write something!");
+  } else {
+    todoList.innerHTML += `<li>${input}</li>`
+  }
 document.querySelector(".input-field").value = ""
  }
 
