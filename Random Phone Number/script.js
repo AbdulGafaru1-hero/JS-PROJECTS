9271a0fd6be2083d49845e36ceb98b6f065811ba
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-07-01 21:44:05 +0000

    button double click disabled

--- Random Phone Number/script.js
+++ Random Phone Number/script.js
@@ -1,35 +1,38 @@
 const generate = document.querySelector(".generate");
 const saveNumber = document.querySelector(".save");
 const result = document.querySelector(".result");
-const inputValue = document.querySelector(".input");
+const input = document.querySelector(".input");
 const networks = document.querySelector(".networks");
 
 
 window.onload = function() {
     if (localStorage) {
-const Number = result.innerHTML += inputValue.value;
-generate.addEventListener("click", function() {
+const phoneNumber = result.innerHTML += input.value;
+
+generate.addEventListener("click", function(event) {
     const randomNumber = (parseInt(Math.random().toFixed(7).replace("0.","")));
     update(randomNumber);
+    event.target.disabled = true; 
 })
+
 
 function update(randomNumber){
     let option = networks.options[networks.selectedIndex];
-    if(option.value === "1") {
-        inputValue.value +=  ` 024${randomNumber}`;
-    } else if (option.value === "2") {
-        inputValue.value +=  ` 050${randomNumber}`;
+    if(option.text === "MTN") {
+        input.value +=  ` 024${randomNumber}`;
+    } else if (option.text === "Vodafone") {
+        input.value +=  ` 050${randomNumber}`;
     } else {
-        inputValue.value +=  ` 027${randomNumber}`;
+        input.value +=  ` 027${randomNumber}`;
     }
 }
 
 saveNumber.addEventListener("click", function(){
-    result.innerHTML += inputValue.value;
-    inputValue.value = "";
+    result.innerHTML += input.value;
+    input.value = "";
 })
 
-window.localStorage.setItem("newNumber",  JSON.stringify("Number"));
+window.localStorage.setItem("newNumber",  JSON.stringify("phoneNumber"));
 
 JSON.parseInt(window.localStorage.getItem('newNumber'));
     }
