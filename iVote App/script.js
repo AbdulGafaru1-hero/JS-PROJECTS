b8683a1f6619ebd65fb80aa1724d980ca3310c56
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-07-02 01:18:52 +0000

    Voter can only vote once feature implemented

--- iVote App/script.js
+++ iVote App/script.js
@@ -7,6 +7,8 @@ let Avotes = 0;
 let Rvotes = 0;
 let Vvotes = 0;
 
+// For Each Method did not work
+
 // buttons.forEach(function(vote) {
 //     vote.addEventListener("click", function (e) {
 //         const voteBtn = e.currentTarget.classList;
@@ -20,16 +22,33 @@ let Vvotes = 0;
 //     })
 // })
 
-function angular(event) {
-    let newVote = Avotes += 1;
-    angularTotal.value = newVote;
-    event.target.disabled = true; 
+let clicked = false;
+
+function alreadyVoted() {
+    let voted = false;
+    if(!voted) {
+        voted = true;
+    }
 }
 
-const react = (event) =>  {
- reactTotal.value = Rvotes++;
- event.target.disabled = true; 
+function angular() {
+    if(!clicked) {
+        let newVote = Avotes += 1;
+        angularTotal.value = newVote;
+        clicked = true;
+        alreadyVoted(voted) 
+    }
+}
+
+function react () {
+    if(!clicked) {
+     reactTotal.value = Rvotes += 1;
+        clicked = true; 
+    }
 }
 function vue() {
-   vueTotal.value = Vvotes++;
+    if (!clicked) {
+        vueTotal.value = Vvotes += 1;
+        clicked = true;     
+    }
 }
